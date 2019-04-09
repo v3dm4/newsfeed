60299f765ae24e5e4b43e1532af281aeac51814d
Denis Travin <disyakidneyshot@gmail.com>
2019-04-09 21:49:31 +0500

    add store and rootReducer

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer)
+
+
+ReactDOM.render(
+<Provider store={store} >
+    <App />
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
